cc2f38338c3f1ba4415801744892ea71e46bac00
Jaruwat Arunpai <jaruwat.arunpai@gmail.com>
2021-05-11 19:19:41 +0700

    2nd Commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+/// TO OPEN
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+/// TO CLOSE
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+/// TO CLOSE BY 'ESC' KEY
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
